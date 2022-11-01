db716f75fc875a86db2897293e2fbc16921238d9
Muhammed Sahal M P <SAHAL2050@GMAIL.COM>
2022-11-01 13:42:48 +0530

    package name changed

--- test/bouncingbutton_test.dart
+++ test/bouncingbutton_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bouncingwidget/bouncingwidget.dart';
+import 'package:bouncerwidget/bouncingwidget.dart';
 
 void main() {
   test('bounces continiously', () {
