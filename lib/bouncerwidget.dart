354d3578ee90c5a4a3d7d8e8b9ddef798797cc6c
Muhammed Sahal M P <SAHAL2050@GMAIL.COM>
2022-11-09 13:33:37 +0530

    fixes

--- lib/bouncerwidget.dart
+++ lib/bouncerwidget.dart
@@ -3,9 +3,20 @@ library bouncingbutton;
 import 'package:flutter/material.dart';
 
 class BouncingWidget extends StatefulWidget {
-  const BouncingWidget({Key? key, required this.child}) : super(key: key);
+  const BouncingWidget(
+      {Key? key,
+      required this.child,
+      this.offsetYaxis = 0.09,
+      this.offsetXaxis = 0,
+      this.milliSecondDuration = 400})
+      : super(key: key);
   final Widget child;
   // A widget to bounce widget and attract users
+  //
+
+  final double offsetYaxis;
+  final int milliSecondDuration;
+  final double offsetXaxis;
 
   @override
   State<BouncingWidget> createState() => _BouncingWidgetState();
@@ -16,11 +27,11 @@ class _BouncingWidgetState extends State<BouncingWidget>
   /// Animation controls
   late final AnimationController _controller = AnimationController(
     vsync: this,
-    duration: const Duration(milliseconds: 400),
+    duration: Duration(milliseconds: widget.milliSecondDuration),
   )..repeat(reverse: true);
   late final Animation<Offset> _animation = Tween<Offset>(
     begin: Offset.zero,
-    end: Offset(0, 0.09),
+    end: Offset(widget.offsetXaxis, widget.offsetYaxis),
   ).animate(_controller);
 
   @override
